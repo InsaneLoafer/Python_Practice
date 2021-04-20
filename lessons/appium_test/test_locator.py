fe0a1e1828e9d8658e7f07f1c55eb13d957c2ca3
InsaneLoafer <Insane_Loafer@163.com>
2021-04-20 21:54:46 +0800

    python practice

--- lessons/appium_test/test_locator.py
+++ lessons/appium_test/test_locator.py
@@ -16,7 +16,7 @@ class TestLocator:
         # desired_caps['platformVersion'] = '6.0'
         desired_caps['deviceName'] = '127.0.0.1:7555'
         desired_caps['appPackage'] = 'com.xueqiu.android'
-        desired_caps['appActivity'] = 'com.xueqiu.android.common.MainActivity'
+        desired_caps['appActivity'] = '.main.view.MainActivity'
         desired_caps['noReset'] = 'true'
         # desired_caps['dontStopAppOnReset'] = "true"
         desired_caps['skipDeviceInitialization'] = "true"
@@ -77,7 +77,38 @@ class TestLocator:
         y_end = int(height * 1/5)
         action.press(x=x1, y=y_start).wait(200).move_to(x=x1, y= y_end).release().perform()
 
+    def test_get_current(self):
+        # 点击搜索框
+        self.driver.find_element_by_id("com.xueqiu.android:id/home_search").click()
+        # 输入搜索内容
+        self.driver.find_element_by_id("com.xueqiu.android:id/search_input_text").send_keys("阿里巴巴")
+        # 定位到第一个搜索结果
+        self.driver.find_element_by_xpath("//*[@resource-id='com.xueqiu.android:id/name' and @text='阿里巴巴").click()
+        # 获取股票代码为09988的价格
+        price = float(self.driver.find_element_by_xpath("//*[@text='09988']/../../..//*[@resource-id='com.xueqiu.android:id/current_price']").text)
+        assert price > 200
 
+    def test_myinfo(self):
+        """
+        1.点击我的，进入个人信息页面
+        2.点击登录，进入到登录页
+        3.输入用户名、密码
+        4.点击登录
+        :return:
+        """
+        self.driver.find_elements_by_android_uiautomator('new UiSelector().text("我的")').click()
+        self.driver.find_elements_by_android_uiautomator('new UiSelector().textContains("账号密码")').click()
+        self.driver.find_elements_by_android_uiautomator('new UiSelector().resourceId("com.xueqiu.android:id/login_account")').send_keys("username")
+        self.driver.find_elements_by_android_uiautomator('new UiSelector().resourceId("com.xueqiu.android:id/login_password")').send_keys("password")
+        self.driver.find_elements_by_android_uiautomator('new UiSelector().resourceId("com.xueqiu.android:id/button_next")').click()
+        self.driver.find_elements_by_android_uiautomator('new UiSelector().resourceId("com.xueqiu.android:id/tab_name").text("我的")').click()
+        self.driver.find_elements_by_android_uiautomator('new UiSelector().resourceId("com.xueqiu.android:id/title_container").childSelector(text("股票"))').click()
+
+    def test_scroll_find(self):
+        self.driver.find_elements_by_android_uiautomator('new UiScrollable(new UiSelector().'
+                                                         'scrollable(true).instance(0)).'
+                                                         'scrollIntoView(new UiSelector().text("黑猫厅长").'
+                                                         'instance(0));')
 
 if __name__ == '__main__':
     pytest.main()
