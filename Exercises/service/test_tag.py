59d00adad07333190bc24c4a83348ff2221085a9
InsaneLoafer <Insane_Loafer@163.com>
2021-05-28 21:59:15 +0800

    python practice

--- Exercises/service/test_tag.py
+++ Exercises/service/test_tag.py
@@ -1,24 +1,49 @@
 #!/user/bin/env python
 # -*- coding: utf-8 -*-
+import pytest
 
 from Exercises.service.tag import Tag
 
 
-def test_tag_get():
-    tag = Tag()
-    token = tag.get_token()
+# todo: 代码冗余
+# todo: 与底层架构耦合太多
+# todo: 封装层次不足，不利于管理
 
-    """获取标签列表"""
-    r = tag.list()
-    assert r.status_code == 200
-    assert r.json()["errcode"] == 0
+class TestTag:
 
-    """创建标签"""
-    res = tag.add(group_name="group1", tags=[{"name": "tag1"}])
-    assert r.status_code == 200
-    assert r.json()['errcode'] == 0
+    def setup_class(self):
+        # todo: 数据清理过程，把测试数据清空或还原
+        self.tag = Tag()
+        self.tag.get_token()
 
-    """校验列表"""
-    r = tag.list()
-    assert r.status_code == 200
-    assert r.json()["errcode"] == 0
+    def test_tag_list(self):
+        """获取标签列表"""
+        r = self.tag.list()
+        assert r.status_code == 200
+        assert r.json()["errcode"] == 0
+
+    @pytest.mark.parametrize("group_name, tag_names", [
+        ["group1", [{"name": "tag1"}]],
+        ["group2", [{"name": "tag2"}, {"name": "tag3"}]]
+    ])
+    def test_tag_get(self, group_name, tag_names):
+        # todo: 完善功能测试
+        """创建标签"""
+        r = self.tag.add(group_name=group_name, tags=tag_names)
+        assert r.status_code == 200
+        assert r.json()['errcode'] == 0
+
+        """校验列表"""
+        r = self.tag.list()
+        assert r.status_code == 200
+        assert r.json()["errcode"] == 0
+
+        group = [group for group in r.json()["tag_group"] if group["group_name"] == group_name][0]
+        tags = [{"name": tag['name']} for tag in r.json()["tag"]]
+        print(group)
+        print(tags)
+        assert group['group_name'] == group_name
+        assert tags == tag_names
+
+    def test_tag_fail(self):
+        pass
